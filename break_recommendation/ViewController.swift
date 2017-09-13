7b1f1b09801d790ebb7ba17c8cd9ffe15e3706d4
a-rom <ayumutanaka2@gmail.com>
2017-09-13 10:53:57 +0900

    initial commit

---
+++ break_recommendation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  break_recommendation
+//
+//  Created by 田中歩 on 2017/09/13.
+//  Copyright © 2017年 田中歩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
